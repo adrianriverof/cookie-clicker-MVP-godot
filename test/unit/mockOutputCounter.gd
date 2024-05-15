f4945f94bd6c8692b5aa09dfd20e7f768c1814d6
adrianriverof <adrianriverof@gmail.com>
2024-05-15 22:38:55 +0200

    test(presenter): display 2 cookies

--- test/unit/mockOutputCounter.gd
+++ test/unit/mockOutputCounter.gd
@@ -6,5 +6,5 @@ var displayed_cookies = 0
 
 
 func update_counter(quantity_of_cookies):
-	displayed_cookies += quantity_of_cookies
+	displayed_cookies = quantity_of_cookies
 
--- test/unit/test_EarnCookie.gd
+++ test/unit/test_EarnCookie.gd
@@ -22,5 +22,11 @@ func test_signal_to_CookieCounter():
 	sut.execute()
 	assert_eq(1, outputCouter.displayed_cookies)
 	
-	
-		
+func test_displaying_two_cookies():
+	var domain = CookieClicker.new()
+	var sut = EarnCookie.new()
+	var outputCouter = MockOutputCounter.new()
+	sut.EarnCookie(domain, outputCouter)
+	sut.execute()
+	sut.execute()
+	assert_eq(2, outputCouter.displayed_cookies)

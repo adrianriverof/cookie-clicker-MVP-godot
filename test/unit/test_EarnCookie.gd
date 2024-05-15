fefaafd75b0cac90e25e6703af07e45d0b6670fb
adrianriverof <adrianriverof@gmail.com>
2024-05-15 22:11:29 +0200

    test(presenter): test if a signal to the cookie counter is applied

--- test/unit/test_EarnCookie.gd
+++ test/unit/test_EarnCookie.gd
@@ -1,6 +1,10 @@
 extends GutTest
 class_name EarnCookieTest
 
+
+
+
+
 func test_EarnCookies():
 	var domain = CookieClicker.new()
 	var sut = EarnCookie.new()
@@ -10,3 +14,13 @@ func test_EarnCookies():
 	
 	assert_eq(1, domain.get_cookies())
 	
+func test_signal_to_CookieCounter():
+	var domain = CookieClicker.new()
+	var sut = EarnCookie.new()
+	var outputCouter = MockOutputCounter.new()
+	sut.EarnCookie(domain, outputCouter)
+	sut.execute()
+	assert_eq(1, outputCouter.get_displayed_cookies())
+	
+	
+		
